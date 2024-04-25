42ed6891ca58cf8dccd734034eec502c70cedc87
lucasevangelistaa <130062327+lucasevangelistaa@users.noreply.github.com>
2024-04-25 13:02:57 -0300

    atualizando

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,4 +1,13 @@
 const hamburguer = document.querySelector('.hamburguer');
 const nav = document.querySelector('.nav');
+const links = document.querySelectorAll('.link')
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"))
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+// Adiciona um event listener para cada link
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        // Fecha o menu removendo a classe "active"
+        nav.classList.remove("active");
+    });
+});
